e93c75193ef74f2c643511629d8c5dc1671feaf7
sehun-kim <rogarithm@gmail.com>
2022-12-11 12:52:11 +0900

    Feat: 로그인하지 않은 사용자가 getProducts 요청 시 인증 예외가 발생한다

--- src/main/java/com/flab/sooldama/domain/product/service/ProductService.java
+++ src/main/java/com/flab/sooldama/domain/product/service/ProductService.java
@@ -4,6 +4,7 @@ import com.flab.sooldama.domain.product.dao.ProductMapper;
 import com.flab.sooldama.domain.product.domain.Product;
 import com.flab.sooldama.domain.product.dto.response.ProductResponse;
 import com.flab.sooldama.domain.product.exception.ProductNotFoundException;
+import com.flab.sooldama.global.exception.AuthenticationFailException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.http.HttpSession;
@@ -13,9 +14,17 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class ProductService {
+
     private final ProductMapper productMapper;
 
+	private static final String USER_EMAIL = "USER_EMAIL";
+
 	public List<ProductResponse> getProducts(Integer offset, Integer limit, Long categoryId, HttpSession session) {
+
+		if (session.getAttribute(USER_EMAIL) == null) {
+			throw new AuthenticationFailException("로그인이 필요한 서비스입니다");
+		}
+
 		List<Product> products = productMapper.selectProducts(offset, limit, categoryId);
 		List<ProductResponse> productResponses = new ArrayList<>();
 
