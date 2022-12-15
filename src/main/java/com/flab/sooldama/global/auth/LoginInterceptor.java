b36a622048ee7cc40d50d60441b1ac40d551a3d6
sehun-kim <rogarithm@gmail.com>
2022-12-15 16:37:23 +0900

    Feat: 중복 로그인, 제품 API 인증 체크 인터셉터 추가

---
+++ src/main/java/com/flab/sooldama/global/auth/LoginInterceptor.java
@@ -0,0 +1,26 @@
+package com.flab.sooldama.global.auth;
+
+import com.flab.sooldama.global.exception.AuthenticationFailException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+	private static final String USER_EMAIL = "USER_EMAIL";
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+		Object handler) throws Exception {
+		HttpSession session = request.getSession();
+
+		if (session.getAttribute(USER_EMAIL) != null) {
+			throw new AuthenticationFailException("이미 로그인한 사용자입니다");
+		}
+
+		return true;
+	}
+}
