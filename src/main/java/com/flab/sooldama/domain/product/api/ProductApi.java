7a2892954328cd1c974fb770f5086dc90bec354c
sehun-kim <rogarithm@gmail.com>
2022-12-16 17:02:07 +0900

    Refactor: 필요 없는 상수값 삭제

--- src/main/java/com/flab/sooldama/domain/product/api/ProductApi.java
+++ src/main/java/com/flab/sooldama/domain/product/api/ProductApi.java
@@ -31,8 +31,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductApi {
     private final ProductService productService;
 
-	private static final String USER_EMAIL = "USER_EMAIL";
-
 	/*
     @RequestParam 어노테이션은 쿼리스트링을 파라미터로 받을 수 있게 도와줍니다.
      */
