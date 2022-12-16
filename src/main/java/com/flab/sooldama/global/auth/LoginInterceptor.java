c05fa62f7def24c2971f056606ebd33e628811a4
sehun-kim <rogarithm@gmail.com>
2022-12-16 17:25:48 +0900

    Fix: 중복 로그인 시 UserAlreadyLoggedinException을 던져야 한다
     - LoginInterceptor가 던질 예외를 바꾸지 않아 테스트가 실패했다.
     - 확인 후 올바르게 바꿨다.

--- src/main/java/com/flab/sooldama/global/auth/LoginInterceptor.java
+++ src/main/java/com/flab/sooldama/global/auth/LoginInterceptor.java
@@ -1,6 +1,7 @@
 package com.flab.sooldama.global.auth;
 
 import com.flab.sooldama.domain.product.exception.AuthenticationFailException;
+import com.flab.sooldama.domain.user.exception.UserAlreadyLoggedinException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -18,7 +19,7 @@ public class LoginInterceptor implements HandlerInterceptor {
 		HttpSession session = request.getSession();
 
 		if (session.getAttribute(USER_EMAIL) != null) {
-			throw new AuthenticationFailException("이미 로그인한 사용자입니다");
+			throw new UserAlreadyLoggedinException("이미 로그인한 사용자입니다");
 		}
 
 		return true;
