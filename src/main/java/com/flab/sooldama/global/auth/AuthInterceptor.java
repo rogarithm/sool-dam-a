f754e457867e14e882b005749c379f2a35c16933
sehun-kim <rogarithm@gmail.com>
2023-01-11 01:13:04 +0900

    Refactor: AuthInterceptor는 인증 세션 키 값을 의존 오브젝트에서 가져온다

--- src/main/java/com/flab/sooldama/global/auth/AuthInterceptor.java
+++ src/main/java/com/flab/sooldama/global/auth/AuthInterceptor.java
@@ -4,20 +4,22 @@ import com.flab.sooldama.domain.product.exception.AuthenticationFailException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
+@RequiredArgsConstructor
 public class AuthInterceptor implements HandlerInterceptor {
 
-	private static final String USER_EMAIL = "USER_EMAIL";
+	private final AuthService authService;
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
 		Object handler) throws Exception {
 		HttpSession session = request.getSession();
 
-		if (session.getAttribute(USER_EMAIL) == null) {
+		if (session.getAttribute(authService.getAuthSessionKey()) == null) {
 			throw new AuthenticationFailException("로그인이 필요한 서비스입니다");
 		}
 
