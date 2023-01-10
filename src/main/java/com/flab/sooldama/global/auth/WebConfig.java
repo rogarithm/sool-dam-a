92fdc49cc627cad50a4028c19bfba9951f72eafe
sehun-kim <rogarithm@gmail.com>
2023-01-11 01:14:10 +0900

    Fix: 인터셉터 설정 빈이 AuthService를 문제 없이 DI 하도록 고친다

--- src/main/java/com/flab/sooldama/global/auth/WebConfig.java
+++ src/main/java/com/flab/sooldama/global/auth/WebConfig.java
@@ -1,12 +1,14 @@
 package com.flab.sooldama.global.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
+@ComponentScan(basePackages = {"com.flab.sooldama.global.auth"})
 public class WebConfig implements WebMvcConfigurer {
 
 	private final AuthInterceptor authInterceptor;
