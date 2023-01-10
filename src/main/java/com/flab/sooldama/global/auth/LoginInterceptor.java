433babf0514de3f97b265f0b59ea9897c207d0ac
sehun-kim <rogarithm@gmail.com>
2023-01-11 01:19:45 +0900

    Refactor: LoginInterceptor는 인증 세션 키 값을 의존 오브젝트에서 가져온다

--- src/main/java/com/flab/sooldama/global/auth/LoginInterceptor.java
+++ src/main/java/com/flab/sooldama/global/auth/LoginInterceptor.java
@@ -4,20 +4,22 @@ import com.flab.sooldama.domain.user.exception.UserAlreadyLoggedinException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
+@RequiredArgsConstructor
 public class LoginInterceptor implements HandlerInterceptor {
 
-	private static final String USER_EMAIL = "USER_EMAIL";
+	private final AuthService authService;
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
 		Object handler) throws Exception {
 		HttpSession session = request.getSession();
 
-		if (session.getAttribute(USER_EMAIL) != null) {
+		if (session.getAttribute(authService.getAuthSessionKey()) != null) {
 			throw new UserAlreadyLoggedinException("이미 로그인한 사용자입니다");
 		}
 
