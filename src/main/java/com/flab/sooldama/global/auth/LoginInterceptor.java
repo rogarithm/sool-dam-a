6783e4d5a2bcd37ea46a3092c23dccc34f6c43e5
sehun-kim <rogarithm@gmail.com>
2023-01-24 00:59:14 +0900

    Refactor: 인증 실패 시 발생 예외를 통일한다
     - 중복 로그인 발생 시 던질 예외만 나머지 인증 로직에서 던지는 예외와
       다르다.
     - 예외 메시지만 다르게 하면 예외를 통일하는 게 일관성 있다고 판단해서
       변경했다.

--- src/main/java/com/flab/sooldama/global/auth/LoginInterceptor.java
+++ src/main/java/com/flab/sooldama/global/auth/LoginInterceptor.java
@@ -1,6 +1,6 @@
 package com.flab.sooldama.global.auth;
 
-import com.flab.sooldama.domain.user.exception.UserAlreadyLoggedinException;
+import com.flab.sooldama.global.exception.AuthenticationFailException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -20,7 +20,7 @@ public class LoginInterceptor implements HandlerInterceptor {
 		HttpSession session = request.getSession();
 
 		if (session.getAttribute(authService.getAuthSessionKey()) != null) {
-			throw new UserAlreadyLoggedinException("이미 로그인한 사용자입니다");
+			throw new AuthenticationFailException("이미 로그인한 사용자입니다");
 		}
 
 		return true;
