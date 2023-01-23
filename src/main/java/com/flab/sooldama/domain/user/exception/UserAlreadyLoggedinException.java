f151f2e952e0f391b7d66ec9f3fee08c14cce91a
sehun-kim <rogarithm@gmail.com>
2023-01-24 01:04:53 +0900

    Refactor: 쓰지 않는 예외를 지운다

--- src/main/java/com/flab/sooldama/domain/user/exception/UserAlreadyLoggedinException.java
+++
@@ -1,8 +0,0 @@
-package com.flab.sooldama.domain.user.exception;
-
-public class UserAlreadyLoggedinException extends RuntimeException {
-
-	public UserAlreadyLoggedinException(String message) {
-		super(message);
-	}
-}
